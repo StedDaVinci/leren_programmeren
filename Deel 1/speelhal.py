6055aae8098ec10e34dbecb8d2533267f505e0d6
Stefcool2007 <143705359+StedDaVinci@users.noreply.github.com>
2023-09-25 12:15:42 +0200

    Update speelhal.py

--- Deel 1/speelhal.py
+++ Deel 1/speelhal.py
@@ -1,13 +1,13 @@
-groepgrootte = 4
-tickets = 7.45
-vrper5min = 0.37
-lengtevr = 45
-per5min = 5
+GROEPGROOTTE = 4
+TICKETS = 7.45
+VRPER5MIN = 0.37
+LENGTEVR = 45
+PER5MIN = 5
 
-hoelangvr = lengtevr / per5min
-kostentickets = groepgrootte * tickets
-perpersoonvr = vrper5min * hoelangvr
-totaalvrkosten = perpersoonvr * groepgrootte
+hoelangvr = LENGTEVR / PER5MIN
+kostentickets = GROEPGROOTTE * TICKETS
+perpersoonvr = VRPER5MIN * hoelangvr
+totaalvrkosten = perpersoonvr * GROEPGROOTTE
 allestotaal = round(totaalvrkosten + kostentickets)
 
 print(f"de totale kosten voor de vr is {totaalvrkosten} euro, de tickets kosten intotaal {kostentickets} euro en intotaal is alles {allestotaal} euro.")
