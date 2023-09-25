f5044f1e87d31f98ab74c45e4fb0c26dd009a0b7
Stefcool2007 <143705359+StedDaVinci@users.noreply.github.com>
2023-09-25 13:11:51 +0200

    feestlunch 2.0

--- Deel 1/feestlunch.py
+++ Deel 1/feestlunch.py
@@ -1,11 +1,15 @@
+aantalcroissanten = int(input("Hoeveel croissantjes wil je? "))
+aantalstokbroodjes = int(input("Hoeveel stokbroden wil je? "))
+aantalkortingsbonnetjes = int(input("Hoeveel geldige kortingsbonnetjes heb je? "))
+
 croissant = 0.39
 stokbrood = 2.78
 kortingsbon = 0.5
 
-aantalcroissantenkosten = croissant * 17
-aantalstokbroodkosten = stokbrood * 2
-aantalkortingsbonnetjes = kortingsbon * 3
+aantalcroissantenkosten = croissant * aantalcroissanten
+aantalstokbroodkosten = stokbrood * aantalstokbroodjes
+aantalkortingsbonnetjes = kortingsbon * aantalkortingsbonnetjes
 totaal = aantalcroissantenkosten + aantalstokbroodkosten
-totaalkorting = totaal - aantalkortingsbonnetjes
+totaalkorting = round(totaal - aantalkortingsbonnetjes)
 
-print(f"het kost intotaal {totaalkorting} met kortingsbonnetjes")
+print(f"De feestlunch kost je bij de bakker {totaalkorting} euro voor de {aantalcroissanten} croissantjes en de {aantalstokbroodjes} stokbroden als de {aantalkortingsbonnetjes} kortingsbonnen nog geldig zijn")
