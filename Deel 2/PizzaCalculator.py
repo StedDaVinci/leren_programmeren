473d1667c3887fa91552f64b5cd952484576d451
Stefcool2007 <143705359+StedDaVinci@users.noreply.github.com>
2023-09-26 10:56:15 +0200

    kleine veranderingen

--- Deel 2/PizzaCalculator.py
+++ Deel 2/PizzaCalculator.py
@@ -31,17 +31,14 @@ while True:
 
 	eindBesteling = input("Wil je betalen of wil je nog verder kiezen? (Betalen/Verder)").lower()
 
-	if eindBesteling == "verder":
-		continue
-
-	else:
+	if eindBesteling != "verder":
 		break
 
-if PKs > 1:
+if PKs > 0:
 	print(f"je order bevat {PKs / PSs, 2} Small pizza('s) en kost €{round(PKs, 2)}")
-if PKm > 1:
+if PKm > 0:
 	print(f"je order bevat {PKm / PSm, 2} Medium pizza('s) en kost €{round(PKm, 2)}")
-if PKl > 1:
+if PKl > 0:
 	print(f"je order bevat {PKl / PSl, 2} Large pizza('s) en kost €{round(PKl, 2)}")
 
 print(f"de totale kosten van je bestelling kost €{round(PKs + PKm + PKl, 2)}")
