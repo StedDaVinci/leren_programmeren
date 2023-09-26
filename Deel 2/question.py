b45ff909e73df581ec113db29121a66bb5764392
Stefcool2007 <143705359+StedDaVinci@users.noreply.github.com>
2023-09-26 11:09:53 +0200

    question.py

---
+++ Deel 2/question.py
@@ -0,0 +1,6 @@
+naam = input("Wat is je naam? ")
+leeftijd = int(input(f"Goedendag {naam}, Wat is je leeftijd? "))
+lievelings_eten = input(f"en als {leeftijd} jarige, wat is je lievelings eten? ")
+drinken = input(f"en wat drink je het liefst bij {lievelings_eten}? ")
+
+print(f"de {leeftijd} jarige {naam} drinkt het liefst {drinken} bij {lievelings_eten}.")
