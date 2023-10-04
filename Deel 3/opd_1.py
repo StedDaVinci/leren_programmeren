d5ac1c5dae7e4df3084de734a369b82a9256f7a1
Stefcool2007 <143705359+StedDaVinci@users.noreply.github.com>
2023-10-04 11:14:31 +0200

    opd 1

--- Deel 3/opd_1.py
+++ Deel 3/opd_1.py
@@ -3,11 +3,17 @@ b = int(input("noem getal B op: "))
 
 if a > b:
 	max = a
+	min = b
 	print(f"A is het grootste getal: {max}")
+	print(f'Het minimum is: {min}')
+	print(f'Het maximum is: {max}')
 
 elif b > a:
 	min = a
+	max = b
 	print(f"A is het kleinste getal: {min}")
+	print(f'Het minimum is: {min}')
+	print(f'Het maximum is: {max}')
 
 else:
-	print("a en b zijn gelijk, er is geen grootste getal.")
+	print("a en b zijn even groot")
