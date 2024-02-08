26cee363d30af36c08a70a935a3bae747f1000c8
Stefcool2007 <143705359+StedDaVinci@users.noreply.github.com>
2024-02-08 09:30:30 +0100

    functions

---
+++ Deel 5/math_operations-start.py
@@ -0,0 +1,19 @@
+# example:
+def increment(nr: float) -> float:
+  return nr + 1
+
+def decrement(nr: float) -> float:
+  return nr - 1
+
+def add(nr1: float, nr2: float) -> float:
+  return nr1 + nr2
+
+def subtract(nr1: float, nr2: float) -> float:
+  return nr1 - nr2
+
+def multiply(nr1: float, nr2: float) -> float:
+  return nr1 * nr2
+
+def divide(nr1: float, nr2: float) -> float:
+  return nr1 / nr2
+
