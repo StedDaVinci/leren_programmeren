04f1774b26fd8bb12f412c51779c9816e42960d1
Stefcool2007 <143705359+StedDaVinci@users.noreply.github.com>
2024-03-11 12:55:09 +0100

    11/03/2024

---
+++ Module 2/deel 2/the list factory.py
@@ -0,0 +1,10 @@
+Lijstje_hoeveelheid = int(input('hoeveel lijstjes wil je?: '))
+hoeveel_in_lijstje1 = int(input("hoeveel in lijstje 1? "))
+hoeveel_in_lijstje2 = int(input("hoeveel in lijstje 2? "))
+hoeveel_in_lijstje3 = int(input("hoeveel in lijstje 3? "))
+
+lijst1 = hoeveel_in_lijstje1 * 1
+lijst2 = hoeveel_in_lijstje2 * 2
+lijst3 = hoeveel_in_lijstje3 * 3
+
+print(f"{lijst1()}")
